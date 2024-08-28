2bb65dc05450e5108f399cb15240097ac51fc3a2
guruprakash-aus <rajendranguruprakash@gmail.com>
2024-08-29 00:25:52 +1000

    [Feature]: Added a Side Bar Nav

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,10 +1,37 @@
 import React from 'react'
+import SideBarDrawer from './SideBarDrawer';
 
 type Props = {}
 
 const Header = (props: Props) => {
   return (
-    <header>Header</header>
+    <header className="sticky top-0 z-10 rounded-md bg-white shadow-sm dark:bg-black">
+    <nav className="p-4 transition-all">
+      <div className="mx-8 flex flex-wrap items-center justify-between">
+        {/* LEFT SIDE */}
+        <div className="flex items-center justify-start">
+          <SideBarDrawer />
+        </div>
+
+        {/* RIGHT SIDE  */}
+
+        <div className="flex items-center space-x-3 md:space-x-6">
+          {/* {user ? (
+            <>
+              <AvatarNav user={user} />
+              <NotifyBtn />
+              <BasketItem />
+            </>
+          ) : (
+            <DialogWrapper isBtn btnTitle="Sign In">
+              <AuthForm callbackUrl="/" />
+            </DialogWrapper>
+          )} */}
+          Right
+        </div>
+      </div>
+    </nav>
+  </header>
   )
 }
 
---
+++ src/components/common/SideBarDrawer.tsx
@@ -0,0 +1,46 @@
+"use client"
+import React from 'react'
+import {
+    Drawer,
+    DrawerContent,
+    DrawerHeader,
+    DrawerTrigger,
+  } from "@/components/ui/drawer"
+import {TiThMenu} from "react-icons/ti"
+import Image from 'next/image';
+import { Avatar, AvatarImage, AvatarFallback } from '../ui/avatar';
+import { Separator } from '../ui/separator';
+import SideNavBar from './SideNavBar';
+import { UserRoutes } from './routes';
+
+type Props = {}
+
+const SideBarDrawer = (props: Props) => {
+  return (
+    <Drawer>
+    <DrawerTrigger className="rounded-full bg-violet-100 p-2 text-violet-500">
+      <TiThMenu size={24} />
+    </DrawerTrigger>
+    <DrawerContent className="fixed inset-0 mt-0 h-screen w-40">
+      <DrawerHeader>
+        <div className="flex justify-center">
+          <Avatar>
+            <AvatarImage src="/logo.png" alt="de-mawo" />
+            <AvatarFallback>US</AvatarFallback>
+          </Avatar>
+          <Image src="/panda.jpg" width={50} height={50} alt="logo"/>
+        </div>
+      </DrawerHeader>
+      <SideNavBar items={UserRoutes} className="p-4" />
+      <Separator className="my-4" />
+      <div className="space-y-3 p-4">
+        Side Nav Items
+        {/* <ThemeToggle/>
+        <LogOutButton withTooltip/> */}
+      </div>
+    </DrawerContent>
+  </Drawer>
+  )
+}
+
+export default SideBarDrawer
